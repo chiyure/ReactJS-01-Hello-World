8bf60ced8ad2fea1e16f42f61176fb6c300427c6
Zoe <K2332226@kingston.ac.uk>
2026-02-06 16:06:29 +0000

    Frontend Development 08 - Component State and Handlers

--- src/components/views/Students.jsx
+++ src/components/views/Students.jsx
@@ -1,9 +1,8 @@
+import { useState } from 'react';
 import { CardContainer, Card } from '../UI/Card.jsx';
 import './Students.scss';
 
-function Students() {
-
- const studentlist = [
+const studentlist = [
     {
       UserID: 275,
       UserFirstname: 'Sholeh',
@@ -146,25 +145,60 @@ function Students() {
     },
   ];
 
+function Students() {
+
+  // INITIALISATION
+
+  const newStudent = {
+    UserFirstname: "Nathan",
+    UserLastname: "Olsson",
+    UserEmail: "K9999999@kingston.ac.uk",
+    UserRegistered: 0,
+    UserLevel: 4,
+    UserYearID: 1,
+    UserUsertypeID: 2,
+    UserImageURL: 'https://images.generated.photos/evdpMs0ZUOoMA0ACfCy98zzmy347YQxRmrPCWHp3v0g/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MzUzMTEyLmpwZw.jpg',
+    UserUsertypeName: "Student",
+    UserYearName: "2022-23",
+  };
+
+  // STATE
+
+  const [students, setStudents] = useState(studentlist);
+
+  // HANDLERS
+
+  const handleAdd = (student) => {
+    student.UserID = Math.floor(10000 * Math.random());
+    setStudents([...students,newStudent]);
+    console.log(`Length of students: ${students.length}`);
+  };
+
+  // VIEW
+
     return ( //wrap in fragment
 
         <>
             <h1>Students</h1>
             <CardContainer> 
                 {
-                studentlist.map((student) => {
+                students.map((student) => {
                     return(
-                    <div className="studentCard" key={student.UserEmail}>
+                    <div className="studentCard" key={student.UserID}>
                     <Card>
                         <p>{student.UserEmail.substring(0,8)}</p>
                         <p>{`${student.UserFirstname} ${student.UserLastname}`}</p>
-                        <img src={student.UserImageURL}/>
+                        <img 
+                          src={student.UserImageURL}
+                          alt={student.UserEmail.substring(0,8)}
+                        />
                     </Card>
                     </div>
                 );
                 }) //callback function; // card container lives in main and in main has cards
                 }
             </CardContainer>
+            <button onClick={() => handleAdd(newStudent)}>Add student</button>
         </>
 
     );
