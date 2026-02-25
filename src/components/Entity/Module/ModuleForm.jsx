4338d67f5003ae3ca3834fe8699a2c4b02af52bc
Zoe <K2332226@kingston.ac.uk>
2026-02-25 02:17:35 +0000

    Frontend Development 13 - Conforming between state and form values

--- src/components/Entity/Module/ModuleForm.jsx
+++ src/components/Entity/Module/ModuleForm.jsx
@@ -4,9 +4,9 @@ import Action from "../../UI/Actions.jsx";
 import "./ModuleForm.scss";
 
 const initialModule = {
-  ModuleName: "",
-  ModuleCode: "",
-  ModuleLevel: 0,
+  ModuleName: null,
+  ModuleCode: null,
+  ModuleLevel: null,
   ModuleYearID: null,
   ModuleLeaderID: null,
   ModuleImageURL: "",
@@ -14,19 +14,38 @@ const initialModule = {
 
 const ModuleForm = ({ onCancel }) => {
   // INITIALISATION
+
+  const conformance = {
+    js2html: {
+      ModuleName: (value) => (value === null ? " " : value),
+      ModuleCode: (value) => (value === null ? " " : value),
+      ModuleLevel: (value) => (value === null ? "0" : value),
+      ModuleYearID: (value) => (value === null ? "0" : value),
+      ModuleLeaderID: (value) => (value === null ? "0" : value),
+      ModuleImageURL: (value) => (value === null ? " " : value),
+    },
+    html2js: {
+      ModuleName: (value) => (value === " " ? null : value),
+      ModuleCode: (value) => (value === " " ? null : value),
+      ModuleLevel: (value) => (value === "0" ? null : parseInt(value)),
+      ModuleYearID: (value) => (value === "0" ? null : parseInt(value)),
+      ModuleLeaderID: (value) => (value === "0" ? null : parseInt(value)),
+      ModuleImageURL: (value) => (value === " " ? null : value),
+    },
+  };
+
   // STATE
   const [module, setModule] = useState(initialModule);
 
   // HANDLERS
   const handleChange = (event) => {
     const { name, value } = event.target;
-    setModule({ ...module, [name]: value });
+    setModule({ ...module, [name]: conformance.html2js[name](value) });
   };
 
   const handleSubmit = () => alert(JSON.stringify(module));
 
   // VIEW
-
   return (
     <div className="moduleForm">
       <Spacer>
@@ -36,7 +55,7 @@ const ModuleForm = ({ onCancel }) => {
             <input
               type="text"
               name="ModuleName"
-              value={module.ModuleName}
+              value={conformance.js2html.ModuleName(module.ModuleName)}
               onChange={handleChange}
             />
           </label>
@@ -46,7 +65,7 @@ const ModuleForm = ({ onCancel }) => {
             <input
               type="text"
               name="ModuleCode"
-              value={module.ModuleCode}
+              value={conformance.js2html.ModuleCode(module.ModuleCode)}
               onChange={handleChange}
             />
           </label>
@@ -55,7 +74,7 @@ const ModuleForm = ({ onCancel }) => {
             Module Level
             <select
               name="ModuleLevel"
-              value={module.ModuleLevel}
+              value={conformance.js2html.ModuleLevel(module.ModuleLevel)}
               onChange={handleChange}>
               <option value="0" hidden>
                 No level selected
@@ -70,8 +89,8 @@ const ModuleForm = ({ onCancel }) => {
             Module Year
             <input
               type="text"
-              name="ModuleYear"
-              value={module.ModuleYearID}
+              name="ModuleYearID"
+              value={conformance.js2html.ModuleYearID(module.ModuleYearID)}
               onChange={handleChange}
             />
           </label>
@@ -80,8 +99,8 @@ const ModuleForm = ({ onCancel }) => {
             Module Leader
             <input
               type="text"
-              name="ModuleLeader"
-              value={module.ModuleLeaderID}
+              name="ModuleLeaderID"
+              value={conformance.js2html.ModuleLeaderID(module.ModuleLeaderID)}
             />
           </label>
 
@@ -90,7 +109,7 @@ const ModuleForm = ({ onCancel }) => {
             <input
               type="text"
               name="ModuleImageURL"
-              value={module.ModuleImageURL}
+              value={conformance.js2html.ModuleImageURL(module.ModuleImageURL)}
               onChange={handleChange}
             />
           </label>
